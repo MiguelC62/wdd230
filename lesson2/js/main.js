bd8ded4e11df6d96ec20664160170b0413ab0be1
MiguelC62 <con18009@byui.edu>
2022-04-29 20:29:52 -0300

    lesson2

---
+++ lesson2/js/main.js
@@ -0,0 +1,6 @@
+// set current year in footer
+const currentDate = new Date();
+document.querySelector('#year').textContent = currentDate.getFullYear();
+
+// set last modification
+document.getElementById("lastupdated").innerHTML = document.lastModified; 

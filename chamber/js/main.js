cfb849133a10c485c03537ed5f7d194d79965f2a
MiguelC62 <con18009@byui.edu>
2022-05-14 20:36:46 -0300

    lesson4

---
+++ chamber/js/main.js
@@ -0,0 +1,22 @@
+// select the elements to manipulate (output to)
+const datefield = document.querySelector(".date");
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
+
+// long, medium, short options ... try them
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
+//
+document.querySelector(
+	"#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
+
+//
+
+function toggleMenu() {
+  document.getElementsByClassName("navigation")[0].classList.toggle("responsive")
+}

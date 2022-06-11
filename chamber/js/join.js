5052f30cb728f82694bedb45935e7fad95837010
MiguelC62 <con18009@byui.edu>
2022-06-11 09:35:16 -0300

    lesson08

--- chamber/js/join.js
+++ chamber/js/join.js
@@ -3,7 +3,7 @@ const datevalue = document.querySelector("#join-date");
 const message = document.querySelector("#date-message");
 const datenow = new Date();
 datevalue.value = datenow;
-console.log(datevalue.value)
+console.log(datevalue.value);
 message.textContent = `Date: <em>${datenow}</em>`;
 message.style.display = "block";
 

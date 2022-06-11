edb8b2f3cbbe73873500667138f5cc05d3f3bf92
MiguelC62 <con18009@byui.edu>
2022-06-11 09:34:17 -0300

    lesson08

--- chamber/js/join.js
+++ chamber/js/join.js
@@ -1,8 +1,9 @@
 // select the elements to manipulate (output to)
 const datevalue = document.querySelector("#join-date");
 const message = document.querySelector("#date-message");
-const now = new Date();
-datevalue.value = now;
-message.textContent = `Date: <em>${now}</em>`;
+const datenow = new Date();
+datevalue.value = datenow;
+console.log(datevalue.value)
+message.textContent = `Date: <em>${datenow}</em>`;
 message.style.display = "block";
 

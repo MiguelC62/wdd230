34eb41828c1211b6567ab20269db89048065af54
MiguelC62 <con18009@byui.edu>
2022-06-16 20:20:29 -0300

    lesson09

---
+++ chamber/js/companies.js
@@ -0,0 +1,71 @@
+const requestURL = 'http://127.0.0.1:5500/companies.json';
+const cards = document.querySelector('.cards');
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['companies'];
+    prophets.forEach(displayCompanies);
+  });
+
+  function displayCompanies(company) {
+    // Create elements to add to the document
+    let card = document.createElement('section');
+    let logo = document.createElement('img');
+    let p1 = document.createElement("p");
+    let p2 = document.createElement("p");
+    let p3 = document.createElement("a");
+
+    card.setAttribute("class", "id")
+    
+    var linkText = document.createTextNode("http://example.com");
+    p3.appendChild(linkText);
+    p3.title = "TASTIL";
+    p3.href = company.website;
+
+    // Change the textContent property of the p element to contain the prophet's date and place birth
+    p1.textContent = `${company.adress}`;   
+    p2.textContent = `${company.phone_number}`;
+    p3.setAttribute('href', company.website);
+
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+    logo.setAttribute('src', company.imageurl);
+    logo.setAttribute('alt', `Image of ${company.name}-${company.membership} Membership`);
+    logo.setAttribute('loading', 'lazy');
+
+    // Add/append the section(card) with the h2 element
+    card.appendChild(logo);
+    card.appendChild(p1);
+    card.appendChild(p2);
+    card.appendChild(p3);
+  
+    // Add/append the existing HTML div with the cards class with the section(card)
+    document.querySelector('div.cards').appendChild(card);
+  }
+
+  //lista
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector(".cards");
+const section = document.querySelector(".id");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+  section.style.flexDirection = "column";
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+  section.style.flexDirection = "row";
+}
+
+
--- chamber/js/join.js
+++ chamber/js/join.js
@@ -1,13 +1,11 @@
 // -----------join page ------------
-var datevalue = document.getElementById('joindate').value;
-console.log(datevalue);
-const myForm = document.getElementById("myForm");
-myForm.addEventListener("submit", varhidden);
-
-function varhidden(){
+function varhidden(event){
     var datevalue = document.getElementById('joindate').value;
     console.log(datevalue);
-    const datenow = new Date();
+    const datenow = event.timeStamp;
     datevalue.setAttribute("value", datenow);
     console.log(datevalue.value);
 }
+const myForm = document.getElementById("myForm");
+myForm.addEventListener("submit", varhidden);
+

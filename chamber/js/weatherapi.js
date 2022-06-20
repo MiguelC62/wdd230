a9bc4a219c38d2538b78e11c21426cd5cab0f763
MiguelC62 <con18009@byui.edu>
2022-06-20 17:53:10 -0300

    lesson10

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -23,14 +23,8 @@ fetch(url)
   });
 
 function chillTemperature(celsius, kmph) {
-	//const celsiusFloat = parseFloat(celsius.textContent);
-//const kmphFloat = parseFloat(kmph.textContent)
-//const fahrenheit = (celsiusFloat * 1.8) + 32;
-//const mph = kmphFloat * 0.62;
 const fahrenheit = (celsius * 1.8) + 32;
 const mph = kmph * 0.62;
-console.log(fahrenheit); // this is temporary for development only
-console.log(mph); // this is temporary for development only
 let chillfactor = 35.74 + 0.6215 * fahrenheit - 35.75 * mph ** 0.16 + 0.4275 * fahrenheit * mph ** 0.16;
 chillfactor = chillfactor.toFixed(1);
 if ( fahrenheit > 50 || mph < 3) {

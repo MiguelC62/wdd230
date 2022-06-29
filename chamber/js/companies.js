7771b60a2124e77d24d1e3c0271c8617f2ac073f
MiguelC62 <con18009@byui.edu>
2022-06-29 18:54:43 -0300

    lesson11

--- chamber/js/companies.js
+++ chamber/js/companies.js
@@ -1,4 +1,4 @@
-const requestURL = 'https://github.com/MiguelC62/wdd230/blob/main/chamber/companies.json';
+const requestURL = 'https://github.com/MiguelC62/wdd230/tree/main/chamber/companies.json';
 const cards = document.querySelector('.cards');
 fetch(requestURL)
   .then(function (response) {

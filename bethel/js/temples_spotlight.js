15b9bf34222a5cb66799ac2887461abccaafce8d
MiguelC62 <con18009@byui.edu>
2022-07-13 20:24:40 -0300

    services

--- bethel/js/temples_spotlight.js
+++ bethel/js/temples_spotlight.js
@@ -1,5 +1,5 @@
 function getWidth() {
-  let x = 3;
+  let x = 1;
   var maxWidth = Math.max(
         document.body.scrollWidth,
         document.documentElement.scrollWidth,
@@ -17,7 +17,7 @@ return x;
 }
 window.addEventListener("resize", getWidth);
 
-const requestURL = 'https://miguelc62.github.io/wdd230/chamber/companies.json';
+const requestURL = 'https://miguelc62.github.io/wdd230/bethel/temples.json';
 const cards = document.querySelector('.spotlight');
 let vect = randomFunction();
 let x = getWidth();
@@ -27,7 +27,7 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    const spot = jsonObject['companies'];
+    const spot = jsonObject['temples'];
     spot.forEach(companies);
   });
  
@@ -42,38 +42,39 @@ fetch(requestURL)
     return arr;
   }
  
-  function companies(company, index) {
+  function companies(temple, index) {
 
     // Create elements to add to the document
     let card = document.createElement('div');
-    let logo = document.createElement('img');
+    let photo = document.createElement('img');
     let h3 = document.createElement("h3");
     let p1 = document.createElement("p");
     let p2 = document.createElement("p");
     let p3 = document.createElement("a");
 
-    card.setAttribute("class", "spot1")
+    card.setAttribute("class", "box spot1")
     
-    var linkText = document.createTextNode("http://example.com");
+    var linkText = document.createTextNode("Temple link");
     p3.appendChild(linkText);
-    p3.title = "TASTIL";
-    p3.href = company.website;
-
-    h3.textContent = `${company.name}`;  
+    p3.title = "Temple";
+    p3.href = temple.website;
+   
+    //temple name
+    h3.textContent = `${temple.name}`;  
 
     // Change the textContent property of the p element 
-    p1.textContent = `${company.adress}`;   
-    p2.textContent = `${company.phone_number}`;
-    p3.setAttribute('href', company.website);
+    p1.textContent = `${temple.adress}`;   
+    p2.textContent = `${temple.phone_number}`;
+    p3.setAttribute('href', temple.website);
 
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    logo.setAttribute('src', company.imageurl);
-    logo.setAttribute('alt', `Image of ${company.name}-${company.membership} Membership`);
-    logo.setAttribute('loading', 'lazy');
+    photo.setAttribute('src', temple.imageurl);
+    photo.setAttribute('alt', `Image of ${temple.name}`);
+    photo.setAttribute('loading', 'lazy');
 
     // Add/append the section(card) with the h2 element
     card.appendChild(h3);
-    card.appendChild(logo);
+    card.appendChild(photo);
     card.appendChild(p1);
     card.appendChild(p2);
     card.appendChild(p3);
